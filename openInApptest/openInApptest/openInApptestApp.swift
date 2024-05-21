0b9cc49815cae1c29c47d066ee6d93b05105c5df
Shreysid2352 <Shreysid2352@gmail.com>
2024-05-21 13:39:40 +0530

    project initialize

---
+++ openInApptest/openInApptest/openInApptestApp.swift
@@ -0,0 +1,17 @@
+//
+//  openInApptestApp.swift
+//  openInApptest
+//
+//  Created by Shrey Sid on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct openInApptestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
